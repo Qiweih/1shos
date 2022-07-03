e482e5547804b7c827ab8bd543231d226dd06d00
Luke <luke@agilelite.com>
2022-07-03 13:07:05 +0200

    first commit

---
+++ _next/static/NIt7orMCuckqGOGmBlTE0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/965-7b9ab99d61395105.js","static/chunks/pages/index-cf246f0b26fb1f4d.js"],"/_error":["static/chunks/pages/_error-d419484d69bbcf53.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
